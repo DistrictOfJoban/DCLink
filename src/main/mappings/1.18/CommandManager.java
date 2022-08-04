e917c0d4b26ab7e9c83fe7ed26cb33ca8a8537ae
LX862 <kenny.mh.hui@gmail.com>
2022-08-04 16:05:40 +0800

    Import fix

--- src/main/mappings/1.18/CommandManager.java
+++ src/main/mappings/1.18/CommandManager.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.server.command.ServerCommandSource;
 
+import java.util.function.Consumer;
+
 public class CommandManager {
 
     public static void registerCommand(Consumer<CommandDispatcher<ServerCommandSource>> callback) {
