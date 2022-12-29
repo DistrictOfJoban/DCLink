ed1caa2c4657ad16a4207a314c5b08f50c84e253
LX862 <kenny.mh.hui@outlook.com>
2022-12-29 17:04:49 +0800

    Me when java 8

--- src/main/java/com/lx/dclink/util/StringHelper.java
+++ src/main/java/com/lx/dclink/util/StringHelper.java
@@ -42,6 +42,6 @@ public class StringHelper {
     }
 
     public static boolean notValidString(String str) {
-        return str == null || str.isBlank();
+        return str == null || str.trim().length() == 0;
     }
 }
